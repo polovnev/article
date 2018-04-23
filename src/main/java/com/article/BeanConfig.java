8a3a6a960622e1d4dde00868642a1599ec95b315
polovnevad@gmail.com <polovnevad@gmail.com>
2018-04-23 14:58:17 +0300

    application starts

--- src/main/java/com/article/BeanConfig.java
+++ src/main/java/com/article/BeanConfig.java
@@ -1,30 +1,73 @@
 package com.article;
 
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.persistence.EntityManagerFactory;
-import javax.xml.crypto.Data;
+import java.util.Properties;
+
 
 /**
  * Created by polovnev on 03.01.18.
  */
 @Configuration
+@EnableTransactionManagement
 public class BeanConfig {
     
-/*
-    
-    @Autowired
-    private EntityManagerFactory entityManagerFactory;
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em
+                = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(new String[] { "com.article.model" });
+        
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
+        
+        return em;
+    }
     
     @Bean
-    public SessionFactory getSessionFactory() {
-        if (entityManagerFactory.unwrap(SessionFactory.class) == null) {
-            throw new NullPointerException("factory is not a hibernate factory");
-        }
-        return entityManagerFactory.unwrap(SessionFactory.class);
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/article");
+        dataSource.setUsername( "root" );
+        dataSource.setPassword( "root" );
+        return dataSource;
     }
-*/
+    
+    @Bean
+    public PlatformTransactionManager transactionManager(
+            EntityManagerFactory emf){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+        
+        return transactionManager;
+    }
+    
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+    
+    Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty(
+                "hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        
+        return properties;
+    }
 }
